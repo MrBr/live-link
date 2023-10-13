26286428b74da70d21f527ccc1a1e6308dae9134
Luka Bracanovic <lbracano@gmail.com>
2023-10-13 10:24:04 +0200

    Define user model type and add fixtures

---
+++ src/modules/user/fixtures/index.ts
@@ -0,0 +1,20 @@
+import { User } from "@/modules/user/types";
+
+export const creators: User[] = [
+  {
+    id: 1,
+    name: "John Doe",
+    description: "A long description",
+    punchline: "Punchline",
+    imgSrc:
+      "https://img.huffingtonpost.com/asset/5bad360e260000360080d8a2.jpeg?ops=scalefit_720_noupscale",
+  },
+  {
+    id: 2,
+    name: "Mary Wunder",
+    description: "An even longer description",
+    punchline: "Wunder punchline",
+    imgSrc:
+      "https://img.freepik.com/free-photo/cute-puppy-wears-sunglasses-summer-fun-generative-ai_188544-15473.jpg",
+  },
+];
---
+++ src/modules/user/types.ts
@@ -0,0 +1,7 @@
+export interface User {
+  id: number;
+  name: string;
+  imgSrc: string;
+  punchline: string;
+  description: string;
+}
